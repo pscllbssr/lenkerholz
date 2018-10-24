bf12efc3d657f45297444bd02de084ae5fb80fdb
Pascal <pa@catatec.ch>
2018-10-24 22:59:32 +0200

    add red background on viewport enter

--- views/Section.js
+++ views/Section.js
@@ -1,11 +1,46 @@
 import './Section.css'
+import ScrollTrigger from 'react-scroll-trigger';
+
 
 export default class extends React.Component {
+
+    constructor(props){
+
+        super(props)
+        this.state = { visible: false }
+    }
+
+    onEnterViewport() {
+        this.setState({
+          visible: true,
+        });
+      }
+     
+      onExitViewport() {
+        this.setState({
+          visible: false,
+        });
+      }
+
     render(){
+
+        const {
+            visible,
+          } = this.state;
+
         return (
             <section className="section">
-                <div className="content-wrapper">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+                    <ScrollTrigger onEnter={this.onEnterViewport.bind(this)} onExit={this.onExitViewport.bind(this)} triggerOnLoad={false}>
+                        <div className={`content-wrapper ${visible ? 'hidden' : ''}`}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
+                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
+                        et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                         Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </div>
+                        
+                    </ScrollTrigger>
             </section>           
         );
     }
