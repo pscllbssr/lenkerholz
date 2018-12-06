cfc5f1b225a379acaae7e04afb7d0a014ec9cca9
Pascal <pa@catatec.ch>
2018-12-06 14:35:28 +0100

    serialize scenes and put in external file

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
-import Hallo from '../components/HelloWorld'
-import Header from '../views/Header'
-import Chapter1 from '../views/Chapter1'
-import Chapter2 from '../views/Chapter2'
-import Chapter3 from '../views/Chapter3'
-import Summary from '../views/Summary'
-import Contact from '../views/Contact'
-import ScrollContainer from '../views/ScrollContainer'
-import { Controller, Scene } from 'react-scrollmagic';
+import ScrollContainer from '../views/ScrollContainer';
 
 import 'normalize.css'
 import 'animate.css'
 import './index.css'
+import scenes from "../views/scenes";
 
 export default class extends React.Component {
     render(){
         return (
-            <ScrollContainer />
+            <ScrollContainer scenes={scenes} />
         );
     }
 }
